00774dd9d0905e52a46e698766cf3f39a9c47cc1
Mukul-Fexle-May2022 <mukul.sharma@fexle.com>
2022-05-25 16:29:40 +0530

    changes  made by dev 1

---
+++ ToStudyConflict.cpp
@@ -0,0 +1,16 @@
+//to know what is conflict 
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello!"; // Changes by dev-1
+	cout<<"I am MUkul"; // Changes by dev-1
+	cout<<"I read"; // Changes by dev-1
+	cout<<"I play"; // Changes by dev-1
+	
+	
+	return 0;
+}
