1cd683469bfbb1947663077667e7f6007fda6d81
Mukul-Fexle-May2022 <mukul.sharma@fexle.com>
2022-05-25 14:32:04 +0530

    main code

---
+++ StudyofConflict.cpp
@@ -0,0 +1,12 @@
+//This is a sample code file of c++ in this file  i will study conflict
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout <<"Hello !\n";
+	cout<<"I am mukul sharma\n";
+	cout<<"I play";
+	return 0;
+}
